4775580079e99d484a14abadfcc671ee6ac1294c
Matthijs Kooijman <matthijs@stdin.nl>
2024-11-28 23:54:52 +0100

    ttn-redis-decoder: Create a procedure from a config message

--- ttn-redis-decoder/consys.py
+++ ttn-redis-decoder/consys.py
@@ -2,6 +2,7 @@ import db
 import os
 import logging
 import urllib
+import uuid
 
 import requests
 from pony import orm
@@ -52,6 +53,72 @@ class ConnectedSystems:
     def process_config_message(self, obj: db.Config):
         system_urn = "{}:system:{}".format(self.urn_root, obj.node_id)
 
+        procedure_uid = uuid.uuid4().urn
+
+        procedure_fields = ""
+
+        for chan_id, channel in obj.data["channel_config"].items():
+            quantity_url = channel["quantity"]
+            # TODO: Explicitly specify name in node?
+            name = os.path.basename(urllib.parse.urlparse(quantity_url).path)
+            uom = channel["unit"]
+            procedure_fields += f"""
+            <swe:field name="{ name }">
+               <swe:Quantity definition="{ quantity_url }">
+                  <swe:uom code="{ uom }"/>
+               </swe:Quantity>
+            </swe:field>
+            """
+
+        # TODO: This seems to give a non-descript 400 error
+        procedure = f"""<?xml version="1.0" encoding="UTF-8"?>
+        <sml:PhysicalSystem gml:id="MY_WEATHER_STATION"
+           xmlns:sml="http://www.opengis.net/sensorml/2.0"
+           xmlns:swe="http://www.opengis.net/swe/2.0"
+           xmlns:gml="http://www.opengis.net/gml/3.2"
+           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+           xmlns:xlink="http://www.w3.org/1999/xlink"
+           xsi:schemaLocation="http://www.opengis.net/sensorml/2.0 http://schemas.opengis.net/sensorml/2.0/sensorML.xsd">
+           <!-- ================================================= -->
+           <!--                  System Description               -->
+           <!-- ================================================= -->
+           <!-- <gml:description>TODO</gml:description> -->
+           <gml:identifier codeSpace="uniqueID">{procedure_uid}</gml:identifier>
+           <gml:name>TODO</gml:name>
+
+            <!-- TODO: Examples define observed properties as inputs, do we need that? -->
+            <!-- TODO: Revisit all definition attributes, want ontologies to use? -->
+
+           <sml:identification>
+             <sml:IdentifierList>
+                <sml:identifier>
+                  <sml:Term definition="http://www.opengis.net/def/ogc/PlatformType">
+                    <sml:label>Platform Type</sml:label>
+                      <!-- TODO: Unhardcode -->
+                      <sml:value>mjs2020</sml:value>
+                    </sml:Term>
+                </sml:identifier>
+             </sml:IdentifierList>
+           </sml:identification>
+
+
+           <sml:outputs>
+              <sml:OutputList>
+                 <sml:output name="data">
+                    <swe:DataRecord>
+                       { procedure_fields }
+                    </swe:DataRecord>
+                 </sml:output>
+              </sml:OutputList>
+           </sml:outputs>
+        </sml:PhysicalSystem>
+        """
+
+        procedure_response = self.post(path="/procedures", data=procedure, content_type="application/sml+xml")
+        assert procedure_response.status_code == 201
+        # This should redirect to the id/path of the added system
+        procedure_path = procedure_response.headers["Location"]
+
         system = {
             "type": "PhysicalSystem",
             # "id": "abcd", # Ignored by OSH?
@@ -59,12 +126,11 @@ class ConnectedSystems:
             "uniqueId": system_urn,
             "label": obj.node_id,
             "description": "TODO",
-            # "typeOf": {
-            #     "href": "https://data.example.org/api/procedures/TP60S?f=sml",
-            #     "uid": "urn:x-myorg:datasheets:ThermoPro:TP60S:v001",
-            #     "title": "ThermoPro TP60S",
-            #     "type": "application/sml+json",
-            # },
+            "typeOf": {
+                "href": self.url + procedure_path,
+                "uid": procedure_uid,
+                "type": "application/sml+json",
+            },
         }
 
         system_response = self.post(path="/systems", json=system, content_type="application/sml+json")
