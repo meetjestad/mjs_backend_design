ac6c9005ed7c1ebdfeb3522c9fe2804781278ddf
Matthijs Kooijman <matthijs@stdin.nl>
2025-11-11 15:16:41 +0100

    ttn-redis-producer: Update
    This:
     - Processes all messages instead of just uplinks
     - Uses maxlen to trim the redis queue after every add
     - Updates debug logging to reflect that the mqtt client does not
       connect until the first message is received

--- ttn-redis-producer/app.py
+++ ttn-redis-producer/app.py
@@ -26,40 +26,56 @@ def get_env_or_file(name, default=None):
 
 
 def main():
-    def on_connect(client, userdata, flags, rc):
-        logging.info("Connected to host, subscribing to uplink messages")
-        client.subscribe("v3/+/devices/+/up")
-
-    def on_message(client, userdata, msg):
-        logging.debug("Received message %s", str(msg.payload))
-
-        try:
-            redis_server.xadd(redis_stream, {
-                "payload": msg.payload,
-                "timestamp": datetime.now(timezone.utc).isoformat(),
-            })
-        except Exception as e:
-            logging.error("Inserting into Redis failed")
-            logging.error(e)
-
-    logging.basicConfig(level=logging.DEBUG)
-
+    redis_url = urlparse(os.environ["REDIS_URL"])
     redis_stream = os.environ["REDIS_STREAM"]
+    try:
+        redis_maxlen = int(os.environ["REDIS_MAXLEN"])
+    except KeyError:
+        redis_maxlen = None
+
     app_id = os.environ.get("TTN_APP_ID")
     access_key = get_env_or_file("TTN_ACCESS_KEY")
     ttn_host = os.environ.get("TTN_HOST", "eu1.cloud.thethings.network")
     ca_cert_path = os.environ.get("TTN_CA_CERT_PATH", "mqtt-ca.pem")
     ttn_port = 8883
 
-    redis_url = urlparse(os.environ["REDIS_URL"])
+    def on_connect(client, userdata, flags, rc):
+        logging.info("Connected to host, subscribing to uplink messages")
+        client.subscribe("v3/+/devices/+/+")
+
+    def on_message(client, userdata, msg):
+        logging.debug("Received message on topic %s: %s", msg.topic, str(msg.payload))
+
+        try:
+            redis_server.xadd(
+                redis_stream,
+                {
+                    "msg": msg.payload,
+                    "timestamp": datetime.now(timezone.utc).isoformat(),
+                    "topic": msg.topic,
+                    "src": "ttn",
+                },
+                # This trims the stream to the given length, but only
+                # removes messages that were acked by all consumer
+                # groups.
+                maxlen=redis_maxlen,
+                approximate=True,
+                ref_policy="ACKED",
+            )
+        except Exception as e:
+            logging.error("Inserting into Redis failed")
+            logging.error(e)
+
+    logging.basicConfig(level=logging.DEBUG)
+
     logging.info(
-        "Connecting Redis to {} on port {}".format(redis_url.hostname, redis_url.port)
+        "Using Redis to {} on port {}".format(redis_url.hostname, redis_url.port)
     )
     redis_server = redis.Redis(
-        host=redis_url.hostname, port=redis_url.port, db=int(redis_url.path[1:] or 0)
+        host=redis_url.hostname, port=redis_url.port, db=int(redis_url.path[1:] or 0), decode_responses=True,
     )
 
-    logging.info("Connecting MQTT to {} on port {}".format(ttn_host, ttn_port))
+    logging.info("Using MQTT to {} on port {}".format(ttn_host, ttn_port))
     mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
