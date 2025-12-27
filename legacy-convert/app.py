a95e984d926ca668a7fb8b6e33b0db19c2e9bb5b
Matthijs Kooijman <matthijs@stdin.nl>
2025-12-27 13:30:10 +0100

    Always use 6 digits for coordinate names
    Adding the f format specifier apparently means to use exactly the
    specified digits (without that, it uses at most that many digits).

--- legacy-convert/app.py
+++ legacy-convert/app.py
@@ -255,7 +255,7 @@ def get_or_create_location_or_foi(sta, lat, lon, is_location):
         # locations as well. Maybe also only merge locations / FOI per
         # thing?
         if lat and lon:
-            name = f"{lat:.{GPS_DIGITS}} / {lon:.{GPS_DIGITS}}"
+            name = f"{lat:.{GPS_DIGITS}f} / {lon:.{GPS_DIGITS}f}"
         else:
             name = "Unknown location"
 
